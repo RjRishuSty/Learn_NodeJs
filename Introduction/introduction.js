2ae3f5b41f1570d039a7afc1ed1bc4f54df1b06f
RjRishuSty <rishurajk59@gmail.com>
2025-01-07 15:44:44 +0530

    add some update

--- Introduction/introduction.js
+++ Introduction/introduction.js
@@ -7,5 +7,16 @@
 // TODO: In Package.json file if you add any js file in script then run command is => npm run objectKey name (example= start)
 // TODO: ---Command  => npm init to install node
 
-
 // TODO: if you want to uninstall any package then => npm uninstall packagename;
+
+// TODO: --------------------------------------- Version ---------------------------------------------------------------
+
+// TODO:  4.18.2
+
+// 1st Part -> 4
+// 2nd Part -> 18
+// 3rd Part -> 2
+
+// TODO: 3rd Part => (Last part) Minor  Fixes  (Optional)
+// TODO: 2nd Part => Recommended Bug Fixes  (Security fix)
+// TODO: 1st Part => (Major Release) 
